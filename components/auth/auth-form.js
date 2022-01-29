0787595df53ddb3455c64aa1b8b4f4e08af1eee9
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2022-01-29 19:05:25 -0300

    Session Provider

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -24,6 +25,7 @@ function AuthForm() {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -44,6 +46,7 @@ function AuthForm() {
 
       if (!result.error) {
         // set some auth state;
+        router.replace("/profile");
       }
     } else {
       try {
@@ -86,5 +89,8 @@ function AuthForm() {
     </section>
   );
 }
+
+
+
 
 export default AuthForm;
